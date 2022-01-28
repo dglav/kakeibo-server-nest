4c7b660ace454c5635cc1037948e6d699ee535c5
Drew <glavich_drew@yahoo.com>
2022-01-28 23:51:28 +0900

    remove console.log

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsModule } from './transactions/transactions.module';
 import { AuthModule } from './auth/auth.module';
-
-console.log('process.env', process.env.DATABASE_HOST);
 @Module({
   imports: [
     TypeOrmModule.forRoot({
