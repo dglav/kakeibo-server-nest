de7052d6ca467ee79a26a13fc45bd6d4a81f94e0
Drew <glavich_drew@yahoo.com>
2022-02-20 17:10:37 +0900

    feat: update AddEnvelopeDto

--- src/envelopes/dto/add-envelope-dto.ts
+++ src/envelopes/dto/add-envelope-dto.ts
@@ -1,6 +1,18 @@
-import { Length } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEnum, Length, Min } from 'class-validator';
+import { TransactionCurrency } from '../../commonTypes';
 
 export class AddEnvelopeDto {
   @Length(1, 255)
   name: string;
+
+  @Length(0, 1000)
+  description: string;
+
+  @Type(() => Number)
+  @Min(0)
+  amount: number;
+
+  @IsEnum(TransactionCurrency)
+  currency: TransactionCurrency;
 }
